f1b678e4aac560a9cdaf525f4f3fba0b952da6cb
martamarkowicz <markowiczmarta5@gmail.com>
2023-05-28 18:07:04 +0200

    Poprawienie wyboru katalogu do badania i wstepne dodanie konfiguracji

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # Narazie mam: podstawowy interfejs, funkcje która odczytuje pliki z katalogu i wyjscie
 # Update: dodana funkcja obliczania skrótu pliku i obliczania jej dla plikow we wskazanym katalogu
-
+# Update: Poprawienie wyboru katalogu do badania i wstepne dodanie konfiguracji
 
 import PySimpleGUI as sg
 import os
@@ -10,7 +10,7 @@ import hashlib
 sciezka_katalogu_domyslna = os.getcwd()
 
 
-# Obliczanie funkcji skrotu
+# Obliczanie funkcji skrótu
 def oblicz_skrót(plik):
     with open(plik, "rb") as f:
         dane = f.read()
@@ -44,8 +44,8 @@ def badaj_katalog(sciezka):
 # Definicja układu interfejsu
 layout = [
     [sg.Text("Witaj w aplikacji do weryfikacji integralności systemu plików!")],
-    [sg.Text("Ścieżka katalogu:", size=(15, 1)),
-     sg.InputText(default_text=sciezka_katalogu_domyslna, key="sciezka_katalogu")],
+    # [sg.Text("Ścieżka katalogu:", size=(15, 1)),
+    #  sg.InputText(default_text=sciezka_katalogu_domyslna, key="sciezka_katalogu")],
     [sg.Button("Badaj katalog"), sg.Button("Konfiguracja"), sg.Button("Wyjście")],
     [sg.Output(size=(60, 10), key="output")]  # Element Output dla wyświetlania tekstu
 ]
@@ -61,17 +61,52 @@ while True:
     if event == sg.WINDOW_CLOSED or event == "Wyjście":
         break
 
-    # Narazie oblicza funkcje skrotu dla wszystkich plikow w wybranym katalogu
+    # Narazie oblicza funkcje skrótów dla wszystkich plików w wybranym katalogu
+    # Poprawiony wybór katalogu
     elif event == "Badaj katalog":
-        sciezka_katalogu = values["sciezka_katalogu"]
-        window["output"].update("")  # Wyczyszczenie pola wyjścia przed wypisaniem nowych danych
-        window["output"].print(f"Badanie integralności katalogu: {sciezka_katalogu}")
-        badaj_katalog(sciezka_katalogu)
+        badanie_layout = [
+            [sg.Text("Badanie")],
+            [sg.Text("Wybierz katalog do badania:", size=(20, 1)), sg.FolderBrowse(key="sciezka_katalogu")],
+            [sg.Button("Badaj"), sg.Button("Powrot")],
+            [sg.Output(size=(60, 10), key="output")]  # Element Output dla wyświetlania tekstu
+        ]
+        badanie_window = sg.Window("Badanie", badanie_layout)
+        while True:
+            badanie_event, badanie_values = badanie_window.read()
+
+            if badanie_event == sg.WINDOW_CLOSED or badanie_event == "Powrot":
+                break
+
+            elif badanie_event == "Badaj":
+                sciezka_katalogu = badanie_values["sciezka_katalogu"]
+                window["output"].update("")  # Wyczyszczenie pola wyjścia przed wypisaniem nowych danych
+                window["output"].print(f"Badanie integralności katalogu: {sciezka_katalogu}")
+                badaj_katalog(sciezka_katalogu)
+
+        badanie_window.close()
 
     # Obsługa zdarzenia "Konfiguracja"
+    # Ustawienie konfiguracji badania katalogu, ale to trzeba pozniej wszystko do bazy danych
     elif event == "Konfiguracja":
-        sciezka_katalogu_domyslna = values["sciezka_katalogu"]
-        # Kod do obsługi konfiguracji
+        konfiguracja_layout = [
+            [sg.Text("Konfiguracja")],
+            [sg.Text("Wybierz katalog do badania:", size=(20, 1)), sg.FolderBrowse(key="konfiguracja_katalogu")],
+            [sg.Button("Zapisz"), sg.Button("Anuluj")]
+        ]
+
+        konfiguracja_window = sg.Window("Konfiguracja", konfiguracja_layout)
+
+        while True:
+            konfiguracja_event, konfiguracja_values = konfiguracja_window.read()
+
+            if konfiguracja_event == sg.WINDOW_CLOSED or konfiguracja_event == "Anuluj":
+                break
+
+            elif konfiguracja_event == "Zapisz":
+                sciezka_katalogu_domyslna = konfiguracja_values["konfiguracja_katalogu"]
+                sg.popup("Zapisano konfigurację")
+
+        konfiguracja_window.close()
 
 # Zamknięcie okna i zakończenie programu
 window.close()
